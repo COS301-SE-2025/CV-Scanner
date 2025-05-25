97e24a55e32bd3e26ba16c2d42e4c10766067635
u23528568 <u23528568@tuks.co.za>
2025-05-25 18:56:08 +0200

    added a namespace

--- Engine/PersonalInfoInterpreter.cpp
+++ Engine/PersonalInfoInterpreter.cpp
@@ -1,5 +1,40 @@
 #include "PersonalInfoInterpreter.h"
+#include <regex>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+#include <vector>
 
-void interpret(std::string str,CVData* data) {
+using namespace std;
+namespace {
+
+    string trim(const string& s) {
+        auto start = s.begin();
+        while (start != s.end() && isspace(*start)) {
+            start++;
+        }
+        auto end = s.end();
+        while (end != start && isspace(*(end - 1))) {
+            end--;
+        }
+        return string(start, end);
+    }
+        vector<string> splitLines(const string& str) {
+        vector<string> lines;
+        size_t pos = 0;
+        while (pos < str.size()) {
+            size_t end = str.find('\n', pos);
+            if (end == string::npos) {
+                lines.push_back(str.substr(pos));
+                break;
+            }
+            lines.push_back(str.substr(pos, end - pos));
+            pos = end + 1;
+        }
+        return lines;
+    }
+}
+
+void PersonalInfoInterpreter::interpret(std::string str,CVData* data) {
 
 }
