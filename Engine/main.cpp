7243c3c0b5c8917a56d4e2679694eb7d17c3bb82
AbdullahP19 <u23726980@tuks.co.za>
2025-05-25 19:50:53 +0200

    updated unified

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
+#include "Engine.h"
+#include "EngineCLI.h"
+#include <windows.h>
 
-int main(){
-
+int main(int argc, char* argv[]) {
+    if(argc > 1) {
+        // CLI Mode
+        EngineCLI cli;
+        cli.executeCommand(argc, argv);
+    } else {
+        // Direct execution mode (for debugging)
+        Engine& engine = Engine::getInstance(8080);
+        engine.start();
+    }
+    return 0;
 }
