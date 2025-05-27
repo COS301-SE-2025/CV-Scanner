88f46a40b724341794d9da349ff7c963b5a3b971
TalhahKarodia <talhahkarodia@gmail.com>
2025-05-27 17:11:58 +0200

    Refactor CVSectionExtractor: streamline interpreter initialization and improve code readability

--- Engine/CVSectionExtractor.cpp
+++ Engine/CVSectionExtractor.cpp
@@ -6,16 +6,23 @@
 #include "PersonalInfoInterpreter.h"
 #include "SkillsInterpreter.h"
 
-CVData* CVSectionExtractor::Extractor (std::string str,CVData* data){
+CVData *CVSectionExtractor::Extractor(std::string str, CVData *data)
+{
+    CVInterpreter *Sinterpreter = new SkillsInterpreter();
+    CVInterpreter *Pinterpreter = new PersonalInfoInterpreter();
+    CVInterpreter *Einterpreter = new ExperienceInterpreter();
+    CVInterpreter *EDinterpreter = new EducationInterpreter();
 
-    CVInterpreter* Sinterpreter = new SkillsInterpreter();
-    CVInterpreter* Pinterpreter = new PersonalInfoInterpreter();
-    CVInterpreter* Einterpreter = new ExperienceInterpreter();
-    CVInterpreter* EDinterpreter = new EducationInterpreter();
+    Sinterpreter->interpret(str, data);
+    Pinterpreter->interpret(str, data);
+    Einterpreter->interpret(str, data);
+    EDinterpreter->interpret(str, data);
 
-    Sinterpreter->interpret(str,data);
-    Pinterpreter->interpret(str,data);
-    Einterpreter->interpret(str,data);
-    EDinterpreter->interpret(str,data);
+    // Clean up if needed (delete interpreters if not used elsewhere)
+    delete Sinterpreter;
+    delete Pinterpreter;
+    delete Einterpreter;
+    delete EDinterpreter;
 
+    return data;
 }
