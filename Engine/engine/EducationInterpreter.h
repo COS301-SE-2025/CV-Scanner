cd852868441714dd401e11aeca7fddf1be2b7e25
Abdullah Pochee <u23726980@tuks.co.za>
2025-05-30 18:06:50 +0200

    dock

---
+++ Engine/engine/EducationInterpreter.h
@@ -0,0 +1,16 @@
+//
+// Created by smart on 5/24/2025.
+//
+
+#ifndef ENGINE_EDUCATIONINTERPRETER_H
+#define ENGINE_EDUCATIONINTERPRETER_H
+
+#include "CVInterpreter.h"
+
+class EducationInterpreter: public CVInterpreter {
+    public:
+    void interpret(std::string str,CVData* data) override;
+};
+
+
+#endif //ENGINE_EDUCATIONINTERPRETER_H
