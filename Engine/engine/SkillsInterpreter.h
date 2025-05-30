ee71c6245990306b3087da700520d38ed16410d4
Abdullah Pochee <u23726980@tuks.co.za>
2025-05-30 18:00:51 +0200

    docker updates

---
+++ Engine/engine/SkillsInterpreter.h
@@ -0,0 +1,16 @@
+//
+// Created by smart on 5/24/2025.
+//
+
+#ifndef ENGINE_SKILLSINTERPRETER_H
+#define ENGINE_SKILLSINTERPRETER_H
+
+#include "CVInterpreter.h"
+
+class SkillsInterpreter: public CVInterpreter{
+public:
+    void interpret(std::string str, CVData* data) override;
+};
+
+
+#endif //ENGINE_SKILLSINTERPRETER_H
