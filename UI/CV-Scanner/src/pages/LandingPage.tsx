402bfa1343ff27abaadb1d06146c32f985a65491
u22491717 <u22491717@tuks.co.za>
2025-06-24 23:49:34 +0200

    Updated colours for Landing page

--- UI/CV-Scanner/src/pages/LandingPage.tsx
+++ UI/CV-Scanner/src/pages/LandingPage.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography, Button, AppBar, Toolbar, IconButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import logo2 from '../assets/logo2.png';
+import logo from '../assets/logo.png';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export default function LandingPage() {
@@ -10,10 +11,10 @@ export default function LandingPage() {
     <Box sx={{ minHeight: '100vh', bgcolor: '#181c2f', color: '#fff', display: 'flex', flexDirection: 'column' }}>
       
         {/* Top App Bar */}
-        <AppBar position="static" sx={{ bgcolor: '#5a88ad', boxShadow: 'none' }}>
+        <AppBar position="static" sx={{ bgcolor: '#1A82AE', boxShadow: 'none' }}>
         <Toolbar sx={{ justifyContent: 'space-between' }}>
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <img src={logo2} alt="Logo" style={{ width: 100 }} />
+            <img src={logo} alt="Logo" style={{ width: 100 }} />
             <Typography variant="h6" sx={{ ml: 2, fontWeight: 'bold' }}>
               CV Scanner
                </Typography>
@@ -59,7 +60,7 @@ export default function LandingPage() {
       </Box>
 
          {/* Footer */}
-      <Box sx={{ textAlign: 'center', py: 2, bgcolor: '#5a88ad', color: '#fff' }}>
+      <Box sx={{ textAlign: 'center', py: 2, bgcolor: '#1A82AE', color: '#fff' }}>
         <Typography variant="body2">&copy; {new Date().getFullYear()} Quantum Stack CV Scanner. All rights reserved.</Typography>
       </Box>
     </Box>
