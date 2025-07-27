5a0c8c313c452db4a44654b3912048c7187039d2
Abdullah Pochee <u23726980@tuks.co.za>
2025-07-28 00:21:29 +0200

    bug fix

--- AI/AI.py
+++ AI/AI.py
@@ -94,6 +94,6 @@ async def upload_cv(file: UploadFile = File(...)):
     })
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8081)
