05241c9e35a308b6e799977d0530249634155660
u23528568 <u23528568@tuks.co.za>
2025-08-03 19:34:45 +0200

    added categories

--- AI/ai_tags.py
+++ AI/ai_tags.py
@@ -1,3 +1,10 @@
 from transformers import pipeline
 
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+categories = {
+    "skills": [],
+    "education": [],
+    "experience": [],
+    "other": []
+}
