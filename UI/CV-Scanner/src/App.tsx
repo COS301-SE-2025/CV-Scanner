2dc2ac8ed4280bb142a166f45faca7b4ce06fee3
u23664828 <u23664828@tuks.co.za>
2025-09-16 22:23:50 +0200

    changed app.tsx

--- UI/CV-Scanner/src/App.tsx
+++ UI/CV-Scanner/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React, { Suspense, lazy } from "react";
-import { GlobalLoaderProvider, useGlobalLoader } from "./hooks/useGlobalLoader";
 import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
 import ResetPassword from './pages/ResetPassword';
@@ -19,11 +18,21 @@ import LandingPage from './pages/LandingPage';
 import Help from './pages/Help';
 import SystemSettingsPage from './pages/SystemSettings';
 import ParsedCVData from './pages/ParsedCVData';
-import LoadingOverlay from "./components/LoadingOverlay";
+import BrandLoading from "./components/BrandLoading";
+import { BrandLoaderProvider, useBrandLoader } from "./hooks/brandLoader";
+
+function BrandLoaderOverlay() {
+  const loader = useBrandLoader();
+  return loader.open ? <BrandLoading /> : null;
+}
+
 
 function App() {
   return (
+    <BrandLoaderProvider>
+      <BrandLoaderOverlay />
     <BrowserRouter>
+     <Suspense fallback={<BrandLoading />}>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
@@ -45,7 +54,9 @@ function App() {
         <Route path="/parsed-cv" element={<ParsedCVData />} />
         <Route path="*" element={<LandingPage />} />
       </Routes>
+       </Suspense>
     </BrowserRouter>
+     </BrandLoaderProvider>
   );
 }
 
