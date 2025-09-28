0355928d59d9717f054d051727b8668bad856f58
u22491717 <u22491717@tuks.co.za>
2025-09-29 00:42:35 +0200

    added cors update

--- API/api/src/main/java/com/example/api/config/CorsConfig.java
+++ API/api/src/main/java/com/example/api/config/CorsConfig.java
@@ -13,10 +13,7 @@ public class CorsConfig {
   @Bean
   public CorsFilter corsFilter() {
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(List.of(
-      "http://localhost:5173",
-      "https://jolly-bay-0e45d8b03.2.azurestaticapps.net"
-    ));
+    config.addAllowedOriginPattern("*");
     config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
