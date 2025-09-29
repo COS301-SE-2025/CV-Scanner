9532e96403dc7032c9482034b04f560515a140bd
Abdullah Pochee <u23726980@tuks.co.za>
2025-09-29 04:53:35 +0200

    added import

--- AI/bart_model.py
+++ AI/bart_model.py
@@ -1,5 +1,9 @@
 
+from typing import Dict, List, Tuple
+from transformers import pipeline
 
+
+_zsc = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
 def classify_text_by_categories(
     text: str,
